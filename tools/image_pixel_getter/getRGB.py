84d20be67784f85e374d899c5531ca270b5d747c
wyskoj <31376393+wyskoj@users.noreply.github.com>
2018-01-17 14:56:05 -0500

    RGB Image Indexer

--- tools/image_pixel_getter/getRGB.py
+++ tools/image_pixel_getter/getRGB.py
@@ -2,5 +2,11 @@ from PIL import Image
 
 im = Image.open("Capture.JPG")
 pix = im.load()
-print im.size
-print pix[1,1]
+for i in range(1,im.size[0]+1):
+		for j in range(1,im.size[1]+1):
+			print(pix[i,j][0])
+			print(",")
+			print(pix[i,j][1])
+			print(",")
+			print(pix[i,j][2])
+			print(":")
